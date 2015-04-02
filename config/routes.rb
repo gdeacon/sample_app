300c8389317c1e797508fc67a6c3a4a81319b116
Garrett Deacon <garrett.deacon@technekes.com>
2015-04-02 13:45:20 -0400

    Finished Layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  
+  root 'pages#home'
+  get 'help' => 'pages#help'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get  'signup' => 'users#new'
+  
+  #get "pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
